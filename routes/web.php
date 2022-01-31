83e825c62df6e9baf831f64f2855c810b5850ad3
hzmhhmdn99 <nurh8307@gmail.com>
2022-02-01 04:11:06 +0800

    dashboard manager

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\LogController;
 use App\Http\Controllers\ReportTableController;
 use App\Http\Controllers\PromotionController;
+use App\Http\Controllers\DashboardController;
 
 
 /*
@@ -135,9 +136,8 @@ Route::post('webhook', [CheckoutController::class, 'stripePay']);
 /******* Manager route start *******/
 /*******************************/
 
-Route::get('/dashboard', function () {
-    return view('layouts.index');
-});
+
+Route::get('/dashboard', [DashboardController::class, 'dashboard']);
 
 Route::get('/searchProduct', 'App\Http\Controllers\CatalogueController@search');
 Route::get('/searchTracking', 'App\Http\Controllers\OrderController@search');
